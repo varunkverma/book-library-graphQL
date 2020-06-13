ee40cf75fa293bf001442643d3e52294aee803fb
varunkverma <varunkverma1996@gmail.com>
2020-06-13 17:07:12 +0530

    graphQL setup and defined book type, root query with a field to get a book by id

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+
+const schema = require("./schema/schema");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({ graphiql: true, schema }));
 
 const PORT = 4000;
 
---
+++ server/schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const books = [
+  { name: "Name of the wind", genre: "Fantasy", id: "1" },
+  { name: "The Final empire", genre: "Fantasy", id: "2" },
+  { name: "The long Earth", genre: "Sci-Fi", id: "3" },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => {
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
