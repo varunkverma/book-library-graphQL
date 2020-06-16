6dc6d2923d235e204650c459d78eaca3047416aa
varunkverma <varunkverma1996@gmail.com>
2020-06-16 23:27:49 +0530

    react client setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import BookList from "./components/book-list";
+function App() {
+  return (
+    <div id="main">
+      <h1>Hello world</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
